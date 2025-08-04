eaaf0cc8e5636cfae58139c86fd79eb0314bdeec
Kaan KIYAK <kaanfnl@gmail.com>
2025-08-04 16:50:28 +0300

    new update hendler

--- router/route.go
+++ router/route.go
@@ -1,25 +1,24 @@
 package router
 
-
 import (
 	"github.com/firebase007/go-rest-api-with-fiber/handler"
 
 	"github.com/firebase007/go-rest-api-with-fiber/middleware"
 
 	"github.com/gofiber/fiber"
-
 )
 
 // SetupRoutes func
-func SetupRoutes (app *fiber.App) {
-	
+func SetupRoutes(app *fiber.App) {
+
 	// Middleware
-	api := app.Group("/api", middleware.AuthReq())  
-	
+	api := app.Group("/api", middleware.AuthReq())
+
 	// routes
 	api.Get("/", handler.GetAllProducts)
 	api.Get("/:id", handler.GetSingleProduct)
 	api.Post("/", handler.CreateProduct)
 	api.Delete("/:id", handler.DeleteProduct)
+	api.Put("/api/product/:id", handler.UpdateProduct)
 
 }
